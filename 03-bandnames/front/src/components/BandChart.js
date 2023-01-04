7fef8800ebc3273dd3542498254ae73dc220140b
adrisanchu <adrian.sanchez@constant-urban.eu>
2023-01-04 17:48:00 +0100

    #03bis - passing data to our chart to update in real time now working! TODO: set specific chart size

--- 03-bandnames/front/src/components/BandChart.js
+++ 03-bandnames/front/src/components/BandChart.js
@@ -1,35 +1,67 @@
 import Chart from 'chart.js/auto';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
+import { SocketContext } from '../context/SocketContext';
+
+let myChart;
 
 export const BandChart = () => {
+	const { socket } = useContext(SocketContext);
+
 	useEffect(() => {
+		socket.on('current-bands', (bands) => {
+			createChart(bands);
+		});
+
+		// triggered when the component is destroyed
+		return () => {
+			myChart.destroy();
+			socket.off('current-bands');
+		};
+	}, [socket]);
+
+	const createChart = (bands = []) => {
 		const ctx = document.getElementById('myChart');
 
-		new Chart(ctx, {
+		// destroy the chart and recreate it to update
+		if (typeof myChart !== 'undefined') myChart.destroy();
+
+		myChart = new Chart(ctx, {
 			type: 'bar',
 			data: {
-				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				labels: bands.map((band) => band.name),
 				datasets: [
 					{
 						label: '# of Votes',
-						data: [12, 19, 3, 5, 2, 3],
+						data: bands.map((band) => band.votes),
 						borderWidth: 1,
 					},
 				],
 			},
 			options: {
+				animation: false,
 				indexAxis: 'y',
-				scales: {
-					x: {
-						stacked: true,
+				scale: {
+					ticks: {
+						precision: 0,
 					},
+					maintainAspectRatio: false
+				},
+				scales: {
 					y: {
 						beginAtZero: true,
+						stacked: true,
+						display: true
 					},
 				},
 			},
 		});
-	}, []);
+
+		// setting a specific size for the chart 
+		// prevents it from "tickle" when updating data!
+		// TODO: use view height and width to make it responsive,
+		// but smaller than the screen! 
+		myChart.resize(600, 600);
+	};
 
 	return <canvas id='myChart'></canvas>;
 };
