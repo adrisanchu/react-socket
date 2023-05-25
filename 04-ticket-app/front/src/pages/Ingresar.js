dbc2c6b3a980da937c9faf9ca28ebf010dc42f0c
adrisanchu <adrian.sanchez@constant-urban.eu>
2023-05-26 00:28:43 +0200

    #04 - add title and subtitle for /ingresar using Ant Design + useNavigate from react router v6

--- 04-ticket-app/front/src/pages/Ingresar.js
+++ 04-ticket-app/front/src/pages/Ingresar.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input, InputNumber } from 'antd';
+import { Button, Form, Input, InputNumber, Divider, Typography } from 'antd';
 import { SaveOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
+
+const { Title, Text } = Typography;
 
 function Ingresar() {
+
+	const navigate = useNavigate();
+
 	const onFinish = (values) => {
 		console.log('Success:', values);
+
+		navigate('/escritorio');
 	};
 
 	const onFinishFailed = (errorInfo) => {
@@ -12,38 +20,45 @@ function Ingresar() {
 	};
 
 	return (
-		<Form
-			name='basic'
-			layout='horizontal'
-			labelCol={{ span: 8 }}
-			wrapperCol={{ span: 14 }}
-			initialValues={{ remember: true }}
-			onFinish={onFinish}
-			onFinishFailed={onFinishFailed}
-			autoComplete='off'
-		>
-			<Form.Item
-				label='Nombre del agente'
-				name='agent'
-				rules={[{ required: true, message: 'Por favor, ingrese su nombre' }]}
+		<>
+			<Title level={2}>Ingresar</Title>
+			<Text>Ingrese su nombre y número de escritorio</Text>
+			<Divider />
+			<Form
+				name='basic'
+				layout='horizontal'
+				labelCol={{ span: 8 }}
+				wrapperCol={{ span: 14 }}
+				initialValues={{ remember: true }}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
+				autoComplete='off'
 			>
-				<Input />
-			</Form.Item>
+				<Form.Item
+					label='Nombre del agente'
+					name='agent'
+					rules={[{ required: true, message: 'Por favor, ingrese su nombre' }]}
+				>
+					<Input />
+				</Form.Item>
 
-			<Form.Item
-				label='Escritorio'
-				name='desktop'
-				rules={[{ required: true, message: 'Ingrese el número de escritorio' }]}
-			>
-				<InputNumber min={1} max={99} />
-			</Form.Item>
+				<Form.Item
+					label='Escritorio'
+					name='desktop'
+					rules={[
+						{ required: true, message: 'Ingrese el número de escritorio' },
+					]}
+				>
+					<InputNumber min={1} max={99} />
+				</Form.Item>
 
-			<Form.Item wrapperCol={{ offset: 8, span: 14 }}>
-				<Button type='primary' htmlType='submit' shape='round'>
-					<SaveOutlined /> Ingresar
-				</Button>
-			</Form.Item>
-		</Form>
+				<Form.Item wrapperCol={{ offset: 8, span: 14 }}>
+					<Button type='primary' htmlType='submit' shape='round'>
+						<SaveOutlined /> Ingresar
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
 	);
 }
 
